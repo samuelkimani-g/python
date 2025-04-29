294eada911d2ca947c4365b6774d15fd32cd767d
samuelkimani-g <samuelgichuru94@gmail.com>
2025-04-29 12:52:35 +0300

    python

---
+++ app.py
@@ -0,0 +1,202 @@
+# print("sam")
+# print("Hello World!")
+
+# student_name = "Sam Kim"
+# students = 12
+# python_students = ["Sam", "Mark", "James"]
+# javascript_students = ("Liam", "Ken","Lorna")
+# student = {
+#     "Name": "Fred",
+#     "age": 18
+# }
+
+# #printing a statement
+# print(student_name)
+
+# a = 3
+# b = 4 
+
+# # print(a + b)
+# add = a + b
+# sub = a - b
+# mul = a * b 
+# div = a / b
+# floor_div = b // a
+# rem = b % a
+# exp = a ** b
+
+# print(add, sub, mul, div, floor_div, rem, exp)
+
+# a = 3
+# b = 4
+
+# a+=1
+# b-=1
+# a*=2
+# b/=2
+# a%=2
+# b//=2
+# a**=4
+
+# print(a)
+# print(b)
+
+# from math import sqrt
+
+# num = 81
+
+# sqrt_result = sqrt(num)
+
+# print(sqrt_result)
+
+# num1 = int(input("Enter a number"))
+# num2 = int(input("Enter another number"))
+
+# print(num1 ** num2)
+
+
+# def greetings(name):
+#     return "Hello " + name
+
+# print(greetings("Sam"))
+# print(greetings("Ken"))
+
+# def add_numbers(*numbers):
+#     return sum(numbers)
+
+
+# print(add_numbers(12,3,4,343,4,3,23,23,23,23,443,343,))
+# print(add_numbers(23,44,34,12,56,34,34,23,45,54,56,67))
+
+# def division_function(a , b):
+#     return a / b
+
+# print(division_function(3, 5))
+
+# def division_function(a, b):
+#     return a / b
+
+# a = float(input("Enter the first number (numerator): "))
+# b = float(input("Enter the second number (denominator): "))
+
+# result = division_function(a, b)
+
+# print("The result is:",result)
+
+#Area of squre
+
+# def area_of_squre (length):
+#     return length * length
+# side = float(input("Enter the length of the squre: "))
+# area = area_of_squre(side)
+
+# print("The area is:",area)
+
+#Area of rectangle
+
+# def area_of_rectangle (length, width):
+#     return length * width
+# length = float(input("Enter the length of the rectangle: "))
+# width = float(input("Enter the width of the rectangle: "))
+
+# area = area_of_rectangle(length, width)
+
+# print("The area is:",area)
+
+#Area of Circle
+
+# def area_of_circle (pi, radius):
+#     return pi * radius **2
+# radius= float(input("Enter the radius of the circle: "))
+# pi = 3.142
+
+# area = area_of_circle(pi, radius)
+
+# print("The area is:",area)
+
+# a = 12
+
+# if a % 2 == 0:
+#     print("even number")
+# else:
+#     print("Odd number")  
+
+
+#CHECKING TYPE OF TRIANGLE
+
+# a = float(input("Enter Side one"))
+# b = float(input("Enter Side two"))
+# c = float(input("Enter Side three"))
+
+# if a==b==c:    #(same as : a==b and a==c and b==c)
+#     print("Equilateral triangle")
+# elif a==b or a==c or b==c:
+#     print("Issosceless triangle")
+# else:
+#     print("Scarline triangle")
+
+#LOOPS
+
+# for num in range(1,5):
+#     print(num) 
+#range(start , end , step)
+
+# students = ["Mark", "Marry", "Sam", "Ken"]
+
+# for student in students:
+#     print(student)
+
+#MULTIPLICATION TABLE OF 5
+
+# for num in range(1,11):
+#     print(f"5 x {num} = {num * 5}")
+
+
+# for num in range(1,11):
+#     ans = (num * 5)
+#     print(f"5 x {num} = {ans}")
+
+# student_name = "George"
+
+# for l in student_name:
+#     print ("l")
+
+
+
+
+
+# countries = ["Uganda", "Kenya", "Tanzania", "Gambia"]
+
+# for country in countries:
+#     if country == "Kenya":
+#         print(f"{country} is Kenya")
+#     else:
+#         print(f"{country} is not Kenya")
+
+# num = 10
+
+# while num < 10: 
+#     print(num)
+#     num+=1     
+
+
+
+
+
+
+# number = 1
+# limit = 10 # You can change this limit as needed
+
+# while number <= limit:
+#     square = number ** 2
+#     print(f"The square of {number} is {square}")
+#     number += 1
+
+
+# for i in range(1,11):
+#     for j in range(i):
+#         print(i, end="")
+#     print()    
+
+
+
---
+++ loops.py
@@ -0,0 +1,137 @@
+#Write a python program using For Loop to iterate through a list of colors and prints each color.
+
+# colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown", "black", "white", "gray", "cyan", "magenta"]
+# for color in colors:
+#     print(color)
+
+#WHILE LOOP
+# Write a short guessing game program using a while loop. The 
+# user should be prompted to guess a number, and you should tell 
+# them whether their guess was correct or wrong after each 
+# guess. The loop should keeping running until the user guesses 
+# the number correctly
+
+
+# secret_number = 7
+# guess = None
+
+# while guess != secret_number:
+#     guess = int(input("Guess a number: "))
+#     if guess == secret_number:
+#         print("Correct! Lucky this round.")
+#     else:
+#         print("Wrong! Unlucky this round.")    
+
+
+
+# Write a short guessing game program using a while loop. The 
+# user should be prompted to guess a number between 1 and 100, 
+# and you should tell them whether their guess was too high or 
+# too low after each guess. The loop should keeping running until 
+# the user guesses the number correctly.
+
+
+
+
+
+
+# guess_number = 56
+# guess = None
+
+# while guess != guess_number:
+#     guess = int(input("Guess a number between 1 and 100: "))
+#     if guess>guess_number:
+#         print("The number is too large")
+#     elif guess<guess_number:
+#         print("The number is too small")
+#     else:
+#         print("You guessed correctly")  
+
+
+
+
+
+
+# Write  a  Python  Program  using  While  Loop  that  calculates  the 
+# sum of numbers from 1 to 500.          
+
+# number = 1
+# total = 0
+
+# while number <= 500:
+#     total +=number
+#     number +=1
+
+# print("The sum of numbers from 1 to 500 is:", total)
+
+
+
+# Write a python program using while loop that counts down from 
+# 5 to 1.
+
+# count = 5
+
+# while count >=1:
+#     print(count)
+#     count -=1
+
+
+
+
+
+# Write  a  python  program  using  while  loop  to  print  all  even 
+# numbers between 1 and 50.
+
+
+
+# i = 1
+
+# while i in range(50):
+#     if i % 2 !=0:
+#         i +=1
+#         continue
+#     print(i)
+#     i +=1
+
+
+
+
+
+# Write a python program using while loop that prints a triangle 
+# pattern of stars.
+
+
+
+# rows = 5 
+# i = 1
+
+# while i <= rows:
+#     print("*" * i)
+#     i+=1
+
+
+
+
+#NESTED LOOPS
+
+# Write a python program using nested loops to print a right angled triangle using stars.
+
+# rows = 5
+
+# for x in range(1, rows + 1):
+#     for j in range(x):
+#          print("*", end="")
+#     print()     
+
+
+
+
+# Create a Python program using nested loops  prints a pattern where each row 
+# has the same number repeated.
+
+
+
+for i in range(1, 6):
+    for j in range(i):
+        print(i, end='')
+    print()
